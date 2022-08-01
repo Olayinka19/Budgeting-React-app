d03f4add353595ddc55e57c9e93f51fb7d89b341
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-08-01 14:40:26 -0400

    Done

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
       <div className="Home">
        <h2>𝕳𝖊𝖓𝖉𝖗𝖎𝖝 𝕭𝖚𝖉𝖌𝖊𝖙ing 𝕬𝖕𝖕</h2>
-        <marquee direction="up"><h3>𝖂𝖊𝖑𝖈𝖔𝖒𝖊 𝖙𝖔 𝖙𝖍𝖊 𝕲𝖗𝖊𝖆𝖙𝖊𝖘𝖙 𝕬𝖕𝖕 𝖔𝖋 𝖆𝖑𝖑 𝖙𝖎𝖒𝖊𝖘!</h3></marquee>
+        <h3>𝖂𝖊𝖑𝖈𝖔𝖒𝖊 𝖙𝖔 𝖙𝖍𝖊 𝕲𝖗𝖊𝖆𝖙𝖊𝖘𝖙 𝕬𝖕𝖕 𝖔𝖋 𝖆𝖑𝖑 𝖙𝖎𝖒𝖊𝖘!</h3>
       
     
         <Time />
